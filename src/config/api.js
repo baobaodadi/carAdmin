4dffb46e8f6bc32a28315644476366b9eb205867
dadi <675834325@qq.com>
2018-11-16 10:58:12 +0800

    delete

--- src/config/api.js
+++
@@ -1,6 +0,0 @@
-import * as constants from './constants';
-
-export default {
-  [constants.ENTITY_DETAIL]: '/taxi/api/order/all',
-  [constants.ENTITY_PAY]: '/taxi/api/order/all',
-};
--- src/config/constants.js
+++
@@ -1,4 +0,0 @@
-// modal type
-export const ENTITY_DETAIL = 'entity_detail';
-export const ENTITY_PAY = 'entity_pay';
-
